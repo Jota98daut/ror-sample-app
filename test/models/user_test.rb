79117e2e0e8ae45f26cd6f6c22c8faf7ddc75c3d
Joel D'Autilio <joeldautilio@gmail.com>
2024-05-06 19:15:21 -0300

    add platform to micropost creation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,11 +4,12 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(username: 'new_user', email: 'new_user@example.com',
                      password: 'abcdef', password_confirmation: 'abcdef')
+    @platform = platforms(:plat)
   end
 
   test 'associated microposts should be destroyed' do
     @user.save!
-    @user.microposts.create!(content: 'Lorem Ipsum')
+    @user.microposts.create!(content: 'Lorem Ipsum', platform: @platform)
     assert_difference 'Micropost.count', -1 do
       @user.destroy
     end
