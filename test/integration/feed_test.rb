4d835d5a4840eb5b28e85ef76a34e8655a657f7e
Joel D'Autilio <joeldautilio@gmail.com>
2024-05-06 19:18:05 -0300

    add confirmation to users

--- test/integration/feed_test.rb
+++ test/integration/feed_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class FeedTest < ActionDispatch::IntegrationTest
   test 'should see microposts from subscribed platforms' do
-    user1 = User.create!(username: 'u1', email: 'u1@example.com', password: '1234567', password_confirmation: '1234567')
-    user2 = User.create!(username: 'u2', email: 'u2@example.com', password: '1234567', password_confirmation: '1234567')
+    user1 = User.create!(username: 'u1', email: 'u1@example.com', password: '1234567', confirmed_at: DateTime.now)
+    user2 = User.create!(username: 'u2', email: 'u2@example.com', password: '1234567', confirmed_at: DateTime.now)
     plat = platforms(:plat)
     mpost = Micropost.create!(content: 'content', platform: plat, user: user2)
     sign_in user1
