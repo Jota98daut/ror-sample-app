1423f412e69b850d5ecfc49cc58e38851504266a
Joel D'Autilio <joeldautilio@gmail.com>
2024-05-06 18:51:05 -0300

    platform controller test

--- test/controllers/platform_controller_test.rb
+++ test/controllers/platform_controller_test.rb
@@ -1,8 +1,17 @@
 require "test_helper"
 
 class PlatformControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get platform_show_url
+  setup do
+    @platform = platforms(:plat)
+  end
+
+  test 'should get show' do
+    get platform_url(@platform)
+    assert_response :success
+  end
+
+  test 'should get index' do
+    get platforms_url
     assert_response :success
   end
 end
